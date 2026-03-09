d20e69179314c8142fe2d1989dc719677d2dc6a2
ftebtw <brandonw22122@gmail.com>
2026-03-09 13:41:58 -0700

    Install Meta Pixel site-wide in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { draftMode } from "next/headers";
+import Script from "next/script";
 
 import { getCmsMessageOverrides } from "@/lib/sanity/content";
 import { Inter, Playfair_Display } from "next/font/google";
@@ -267,6 +268,27 @@ export default async function RootLayout({
         ))}
       </head>
       <body className="font-sans antialiased">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2316561212086063');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2316561212086063&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <script
           dangerouslySetInnerHTML={{
             __html: 'try{var t=localStorage.getItem("dsdc-theme");var e=document.documentElement;if(t==="dark")e.classList.add("dark");else if(t==="light")e.classList.remove("dark");}catch(n){}',
